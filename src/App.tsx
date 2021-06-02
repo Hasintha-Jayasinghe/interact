8a5116378c55c484b94cbd6c4f5416c5813b4e58
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2021-06-02 16:56:28 +0530

    mixed up thr routes again

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [verified, setVerified] = useState(false);
+  const [attempts, setAttempts] = useState(4);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Super secret site</h1>
+      {attempts === 0 ? (
+        <h1>Yeah sorry but you ran out of attempts!</h1>
+      ) : (
+        <form className="form" onSubmit={e => e.preventDefault()}>
+          <h4>Enter the password:</h4>
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            className="password-input"
+            type="password"
+          />
+          <button
+            onClick={() => {
+              if (password === '1900') {
+                setVerified(true);
+              } else {
+                setAttempts(attempts - 1);
+                alert(`WRONG PASSWORD!! YOU HAVE ${attempts} MORE ATTEMPTS`);
+              }
+            }}
+            className="btn"
+          >
+            Gimme the secret link!
+          </button>
+        </form>
+      )}
+      {verified && (
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          SUPER SECRET LINK:{' '}
+          <a href="https://drive.google.com/file/d/1LW5MlBN8A3L2ZP2iu-jrICTcdW9OQphw/view?ts=60b73771">
+            https://drive.google.com/file/d/1LW5MlBN8A3L2ZP2iu-jrICTcdW9OQphw/view?ts=60b73771
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
