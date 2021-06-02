279e49734378dc45352d7cc3b3d40183702fcc64
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2021-06-02 21:25:01 +0530

    mixed up thr routes again

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,29 @@ import './App.css';
 function App() {
   const [password, setPassword] = useState('');
   const [verified, setVerified] = useState(false);
-  const [attempts, setAttempts] = useState(4);
 
   return (
     <div className="App">
       <h1>Super secret site</h1>
-      {attempts === 0 ? (
-        <h1>Yeah sorry but you ran out of attempts!</h1>
-      ) : (
-        <form className="form" onSubmit={e => e.preventDefault()}>
-          <h4>Enter the password:</h4>
-          <input
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            className="password-input"
-            type="password"
-          />
-          <button
-            onClick={() => {
-              if (password === '1900') {
-                setVerified(true);
-              } else {
-                setAttempts(attempts - 1);
-                alert(`WRONG PASSWORD!! YOU HAVE ${attempts} MORE ATTEMPTS`);
-              }
-            }}
-            className="btn"
-          >
-            Gimme the secret link!
-          </button>
-        </form>
-      )}
+      <form className="form" onSubmit={e => e.preventDefault()}>
+        <h4>Enter the password:</h4>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className="password-input"
+          type="password"
+        />
+        <button
+          onClick={() => {
+            if (password === '1900') {
+              setVerified(true);
+            }
+          }}
+          className="btn"
+        >
+          Gimme the secret link!
+        </button>
+      </form>
       {verified && (
         <p>
           SUPER SECRET LINK:{' '}
